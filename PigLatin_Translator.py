b7716ab5a16581b574719faa6c747a05b050362c
ektabhatt08 <bhattekta75@gmail.com>
2021-01-09 20:20:04 +0530

    initial commit

---
+++ PigLatin_Translator.py
@@ -0,0 +1,38 @@
+
+# Get sentence from user
+
+original = input("Please enter a sentence: ").strip().lower()
+
+# Split sentence to words
+
+words = original.split()
+
+# Loop through words and convert to pig latin
+
+new_words=[]
+for word in words:
+    if word[0] in 'aeiou':
+        new_word = word + 'yay'
+        new_words.append(new_word)
+    else:
+        vowel_pos= 0
+        for letter in word:
+            if letter not in 'aeiou':
+                vowel_pos = vowel_pos + 1
+            else:
+                break
+        cons = word[ : vowel_pos]
+        the_rest= word[vowel_pos : ]
+        new_word = the_rest + cons + 'ay'
+        new_words.append(new_word)
+
+
+# Stick words back together
+
+output = " ".join(new_words)
+
+# Display output
+
+print(output)
+        
+        
